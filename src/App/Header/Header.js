e47f97df452f080000b726fae91edda7dbffeef8
Nadim Salloum <nsalloum@sensational.ch>
2019-04-11 10:06:52 +0200

    add reactstrap nav and bootstrap css

--- src/App/Header/Header.js
+++ src/App/Header/Header.js
@@ -1,24 +1,43 @@
 import React, {Component} from "react";
-import {Link} from "react-router-dom";
 import './Header.css';
+import { Collapse, Navbar, NavbarToggler, NavbarBrand, Nav, NavItem, NavLink } from 'reactstrap';
 
 class Header extends Component {
+  constructor(props) {
+    super(props);
+
+    this.toggleNavbar = this.toggleNavbar.bind(this);
+    this.state = {
+      collapsed: true
+    };
+  }
+
+  toggleNavbar() {
+    this.setState({
+      collapsed: !this.state.collapsed
+    });
+  }
   render() {
     return (
-      <nav className="navbar navbar-default">
-        <div className="container">
-          <Link to="/" className="navbar-brand">Projectarr</Link>
-          <ul className="nav navbar-nav navbar-right">
-            <li className="nav-item">
-                <Link to="/projects" className="nav-link">projects</Link>
-            </li>
-            <li className="nav-item">
-                <Link to="/employees" className="nav-link">employees</Link>
-            </li>
-          </ul>
-          <Link to="/logout" className="nav-link">logout</Link>
-        </div>
-      </nav>
+      <div>
+        <Navbar light>
+          <NavbarBrand href="/" className="mr-auto">projectarr</NavbarBrand>
+          <NavbarToggler onClick={this.toggleNavbar} className="mr-2" />
+          <Collapse isOpen={!this.state.collapsed} navbar>
+            <Nav navbar>
+              <NavItem>
+                <NavLink href="/projects/">Projects</NavLink>
+              </NavItem>
+              <NavItem>
+                <NavLink href="/employees/">Employees</NavLink>
+              </NavItem>
+              <NavItem>
+                <NavLink href="/logout/">logout</NavLink>
+              </NavItem>
+            </Nav>
+          </Collapse>
+        </Navbar>
+      </div>
     )
   }
 }
